0c7254b734c03951cab25ce90eb24477233910d3
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-10-12 17:12:54 +0300

    Task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+/**
+ * Retrieves a map of grocerie names and their quantity
+ */
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}
