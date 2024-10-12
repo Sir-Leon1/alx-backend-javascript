04ce647506eb2a0812d02c194f5e83816529ff18
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-10-12 16:52:15 +0300

    Task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+/**
+ * Createa a buffer array with a given position set to a given value
+ */
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buf = new DataView(new ArrayBuffer(length), 0, length);
+  buf.setInt8(position, value);
+  return buf;
+}
