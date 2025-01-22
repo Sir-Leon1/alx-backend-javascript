412cc6140d94cdbc9977ccd8fbd8733facda541c
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2025-01-22 14:05:35 +0300

    t2

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,16 @@
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
-});
-
-process.on('exit', () => {
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
-});
+  });
+}
